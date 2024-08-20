0942547b77b74336cdb02686e8f406e8c9f2c367
alanisgodoy <alanisgodoy90@gmail.com>
2024-08-19 21:00:40 -0300

    Add files via upload

--- eventos.js
+++ eventos.js
@@ -1,3 +1,8 @@
 document.getElementById('contenedor').addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
+
+document.querySelector('button').addEventListener('click', function(event) {
+    event.stopPropagation(); // Evita que el evento se propague al div
+    alert('Hola!');
+});
